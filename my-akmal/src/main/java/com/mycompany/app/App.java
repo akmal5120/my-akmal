17372ace3492fb91bd84b881f25e51091e74b6f1
akmal5120 <48241656+akmal5120@users.noreply.github.com>
2021-07-18 21:16:56 +0530

    Update App.java

--- my-akmal/src/main/java/com/mycompany/app/App.java
+++ my-akmal/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Akmal !" );
+        System.out.println( "Hello Akmal how r u !" );
     }
 }
